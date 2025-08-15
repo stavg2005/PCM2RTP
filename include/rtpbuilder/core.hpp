c66d09f4ceaa0db4348169d3c0b613852bbc1c68
Stav Granek <36859590+stavg2005@users.noreply.github.com>
2025-08-15 18:03:03 +0300

    this starts to get annoying

--- include/rtpbuilder/core.hpp
+++ include/rtpbuilder/core.hpp
@@ -1,2 +1,13 @@
 #pragma once
-int TransmitFile(int argc, char* argv[]);
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <filesystem>
+#include <iostream>
+#include <rtpbuilder/SessionManager.hpp>
+
+
+std::unique_ptr<SessionManager>
+TransmitWavFile(boost::asio::io_context &io, uint16_t localPort,
+                const std::string &remoteIp, uint16_t remotePort,
+                const std::filesystem::path &filePath,
+                const std::string &fileName);
